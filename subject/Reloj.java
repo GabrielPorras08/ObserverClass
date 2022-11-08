ac57cea0352c95b19bf0b2fdf86bb131ac0de79f
Luis Diego <luisitobando820@gmail.com>
2022-11-08 14:50:14 -0600

    Se agreagron los metodos abstractos.

--- subject/Reloj.java
+++ subject/Reloj.java
@@ -1,6 +1,6 @@
 package subject;
 
-public class Reloj {
+public class Reloj implements Observable{
     private int seconds;
     private int minutes;
     private int hours;
@@ -16,5 +16,20 @@ public class Reloj {
     public void show(){
         System.out.println(hours + ":" + minutes + ":" + seconds );
         System.out.println();
+    }
+
+    @Override
+    public void attach(Observable o) {
+
+    }
+
+    @Override
+    public void detach(Observable o) {
+
+    }
+
+    @Override
+    public void notifyObserver() {
+
     }
 }
